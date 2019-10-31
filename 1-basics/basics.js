52ae6c28c7fd03e504452d3ebd24935eda7ccbee
vlaxroque <vin.ombongi@gmail.com>
2019-10-31 17:23:51 +0300

    my commit

--- 1-basics/basics.js
+++ 1-basics/basics.js
@@ -104,7 +104,7 @@ describe("basics", function() {
             // written in way that we don't have to write all the zeros AFTER the point.
             // Write it as a string so that the .length property can be checked.
             // ✏️ EDIT NEXT LINE ✏️
-            const someNumber = "...";
+            const someNumber = "1e1";
             ////////////////////////////////////////////////////////////////////
 
             expect(typeof someNumber).to.equal("string");
