aa323ffa34a637db775c219a3365d09f3a950b7e
vlaxroque <vin.ombongi@gmail.com>
2019-10-31 18:34:34 +0300

    i commit

--- 1-basics/basics.js
+++ 1-basics/basics.js
@@ -355,7 +355,9 @@ describe("basics", function() {
             // You can create objects that reference themselves.
             // But you need two steps to do it.
             // ✏️ ADD CODE HERE ✏️
+            const object={};
 
+            object.object=object;
             ////////////////////////////////////////////////////////////////////
 
             expect(object.object).to.equal(object);
